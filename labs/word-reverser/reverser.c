f6f97985c05721b44d98fdfc69567ef696ca40c3
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2021-02-24 03:17:08 +0000

    Word-reverser lab solved

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+void reverse_and_print(int length, char arr[]) {
+    int i, tmp;
+    for (i = 0;  i < length/2; i++) {
+	    tmp = arr[i];
+	    arr[i] = arr[length - i - 1];
+	    arr[length - i - 1] = tmp;
+    }
+    for (int i = 0;  i < length; i++) {
+	    printf("%c" , arr[i]);
+    }
+}
+
 int main(){
-    // Place your magic here
+    char currentChar;
+    char currentWord[255];
+    int wordIndex = 0;
+
+    while((currentChar = getchar()) != EOF){
+        if(currentChar == '\n'){
+            reverse_and_print(wordIndex, currentWord);
+            printf("\n");
+            break;
+        }else if(currentChar == ' ' || currentChar == '\t'){
+            reverse_and_print(wordIndex, currentWord);
+            printf("%c", currentChar);
+            wordIndex = 0;
+        }else{
+            currentWord[wordIndex] = currentChar;
+            wordIndex++;
+        }
+    }
     return 0;
 }
