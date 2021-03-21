d80f675db789e9cb04e7f805bb0d956f429c6e84
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2021-03-21 17:37:06 +0000

    Solve string library lab

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char *str);
+char *add(char *origin, char *addition);
+int find(char *origin, char *substr);
+int mystrcmp(char *string1, char *string2);
+
+int main(int argc, char**argv) {
+
+    if(argc<2){
+        printf("No command detected.\n");
+        return 0;
+    }
+
+    if(mystrcmp(argv[1],"-add")==0){
+        if(argc==4){
+            printf("Initial Length : %d\n", mystrlen(argv[2]));
+            char* added = add(argv[2], argv[3]);
+            printf("New String     : %s\n", added);
+            printf("New Length     : %d\n", mystrlen(added));
+        }else{
+            printf("Missing parameters for add.\n");
+        }
+        return 0;
+    }
+    
+    if(mystrcmp(argv[1], "-find")==0){
+        if(argc==4){
+            int position = find(argv[2], argv[3]);
+            if(position>-1){
+                printf("[%s] string was found at [%d] position.\n", argv[3], position);
+            }else{
+                printf("[%s] string was not found.\n", argv[3]);
+            }
+            
+        }else{
+            printf("Missing parameters for find.\n");
+        }
+        return 0;
+    }
+
+    printf("No valid command detected.\n");
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,68 @@
+#include <stdlib.h>
 
+int mystrcmp(char *string1, char *string2);
+void cleanarray(char *toClean);
 
 int mystrlen(char *str){
-    return 0;
+    int res = 0;
+    while(str[res]!='\0'){
+        res++;
+    }
+    return res;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *add(char *origin, char *addition){
+    int l1 = mystrlen(origin);
+    int l2 = mystrlen(addition);
+    char* res = (char *)malloc(l1+l2+1);
+    int i = 0;
+    for(i = 0; i < l1; i++){
+        res[i] = origin[i];
+    }
+    for(i; i < l1+l2; i++){
+        res[i] = addition[i-l1];
+    }
+    return res;
 }
 
-int mystrfind(char *origin, char *substr){
+int find(char *origin, char *substr){
+    int originLen = mystrlen(origin);
+    int subIndex = 0;
+    char* buffer = (char*)malloc(originLen+1);
+    for(int i = 0;i<originLen; i++){
+        if((origin[i] == substr[subIndex])){
+            buffer[subIndex] = origin[i];
+            if(mystrcmp(buffer, substr)==0){
+                return i - mystrlen(buffer) + 1;
+            }
+            subIndex++;
+        }else{
+            cleanarray(buffer);
+            subIndex=0;
+        }
+    }
+    return -1;
+}
+
+void cleanarray(char *toClean){
+    int i = 0;
+    while(toClean[i]!='\0'){
+        toClean[i] = '\0';
+        i++;
+    }
+}
+
+int mystrcmp(char *string1, char *string2){
+    int len = mystrlen(string1);
+    if(len != mystrlen(string2)){
+        return -1;
+    }
+
+    for(int i = 0;i<len; i++){
+        if(string1[i]!=string2[i]){
+            return -1;
+        }
+    }
+
     return 0;
 }
