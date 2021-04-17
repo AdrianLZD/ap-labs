605c513070eba9e7446ec8d2b5b3e00c915ac9ad
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2021-04-17 21:36:41 +0000

    Advanced logger lab solved

--- labs/advanced-logger/logger.h
+++ labs/advanced-logger/logger.h
@@ -1,7 +1,9 @@
 // Logger
 
 int initLogger(char *logType);
+int printLog(char *color, char *type, const char *format, va_list args);
 int infof(const char *format, ...);
 int warnf(const char *format, ...);
 int errorf(const char *format, ...);
 int panicf(const char *format, ...);
+
